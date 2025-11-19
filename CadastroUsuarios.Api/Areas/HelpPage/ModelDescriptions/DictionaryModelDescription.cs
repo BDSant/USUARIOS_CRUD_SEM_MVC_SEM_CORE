bee1df24206924a53a6ae9fc8fc2564eda40ac14
Benne <bennesantana@hotmail.com>
2025-11-18 22:05:05 -0300

    Remoção de pasta do projeto de API

--- CadastroUsuarios.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CadastroUsuarios.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- CadastroUsuarios.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CadastroUsuarios.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
